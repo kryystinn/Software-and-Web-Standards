56e7bc374384409339f26e86a9cb549384bf9ae2
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-12-08 22:37:09 +0100

    ehersisio siete

---
+++ practica4/Ejercicio 5/CalculadoraRPN.js
@@ -0,0 +1,127 @@
+class CalculadoraRPN {
+
+    constructor(){
+        this.memory = 0;
+    }
+
+    get input(){
+        return document.getElementById("result1");
+    }
+
+    setMemVal(val){
+        this.memory = val;
+    }
+
+    dis(value){
+        if (this.input.value == "0"){
+            this.clear();
+        }
+        this.input.value += value;
+    }
+
+    clickMrc(){
+        this.input.value = this.memory;
+    }
+
+    clickMMinus(){
+        this.setMemVal(Number(this.memory) - Number(this.input.value));
+        this.clear();
+    }
+
+    clickMPlus(){
+        this.setMemVal(Number(this.memory) + Number(this.input.value));
+        this.clear();
+    }
+
+    dec(){
+        if (this.input.value == ""){
+            this.input.value = "0";
+        }
+        this.input.value += ".";
+    }
+
+    sum(){
+        this.input.value += "+";
+    }
+
+    subs(){
+        this.input.value += "-";
+    }
+
+    mult(){
+        this.input.value += "*";
+    }
+
+    div(){
+        this.input.value += "/";
+    }
+
+    clear(){
+        this.input.value = "";
+    }
+
+    enter(){
+        this.input.value = eval(this.input.value);
+    }
+    clickMc(){
+        this.setMemVal(0);
+    }
+
+    power(){
+        this.input.value = Math.pow(this.input.value, 2);
+    }
+
+    powerValue(){
+        this.input.value += "**";
+    }
+
+    sin(){
+        this.input.value = Math.sin(this.input.value);
+    }
+
+    cos(){
+        this.input.value = Math.cos(this.input.value);
+    }
+
+    tan(){
+        this.input.value = Math.tan(this.input.value);
+    }
+
+    root(){
+        this.input.value = Math.sqrt(this.input.value);
+    }
+
+    tenPowerValue(){
+        this.input.value = Math.pow(10, this.input.value);
+    }
+
+    log(){
+        this.input.value = Math.log(this.input.value);
+    }
+
+    exp(){
+        this.input.value += "e+";
+    }
+
+    mod(){
+        this.input.value += "%";
+    }
+
+    pi(){
+        this.input.value = Math.PI;
+    }
+
+    fact(){
+		var res = 1.0;
+		for (var i = this.input.value; i > 0; i--) { 
+			res *= i;
+		}
+		this.input.value = res;
+    }
+    
+    changeSign(){
+        this.input.value = this.input.value * -1;
+    }
+}
+
+var crpn = new CalculadoraRPN();
---
+++ practica4/Ejercicio 7/Ejercicio7.js
@@ -0,0 +1,49 @@
+class Ejercicio7 {
+
+    constructor(){
+        this.arbolDomMostrado = false;
+    }
+
+    ocultarElementos(){
+        $("h2:first").hide();
+        $("p:first").hide();
+    }
+
+    mostrarElementos(){
+        $("h2:first").show();
+        $("p:first").show();
+    }
+
+    modificarTitulo(){
+        var newTitle = $("#newTitle").val();
+        $("#titleChange").text(newTitle);
+    }
+
+    addTexto(){
+        var text = $("#newCode").val();
+        $("#add").append(text);
+    }
+
+    delete(){
+        $(".delete").remove();
+    }
+
+    arbolDom(){
+        if (!this.arbolDomMostrado){
+            $("*", document.body).each(function(){
+                var etiquetaPadre = $(this).parent().get(0).tagName;
+                $(this).prepend(document.createTextNode( "Etiqueta padre : <"  + etiquetaPadre + "> elemento : <" + $(this).get(0).tagName +"> valor: "));
+            })
+            this.arbolDomMostrado = true;
+        }
+    }
+
+    tabla(){
+        var rows = $("table tr").length;
+        var columns = $("table tr th").length;
+        $("#filascolumnas").val("Número de filas: " + rows + "; Número de columnas: " + columns);
+    }
+
+}
+
+var e = new Ejercicio7();
